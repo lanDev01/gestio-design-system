31660e833d19c0dfd8b10584e459747696599bc8
LanDev01 <alnjr0001@outlook.com>
2026-02-09 20:22:08 -0300

    feat(gds): update ng-package and add main styles file

--- projects/gds/src/public-api.ts
+++ projects/gds/src/public-api.ts
@@ -1,7 +1,11 @@
-/*
- * Public API Surface of gds
- */
+// Components / Modules
+export * from './lib/gds-button/gds-button.component';
+export * from './lib/gds-button/gds-button.module';
 
-export * from './lib/gds.service';
+// Legacy bundle module
 export * from './lib/gds.component';
 export * from './lib/gds.module';
+
+// Tokens
+export * from './lib/tokens/colors';
+export * from './lib/tokens/typography';
