855a5ef341fee3875523c0614e7dff7ba32422cd
LanDev01 <alnjr0001@outlook.com>
2026-02-09 22:55:41 -0300

    feat(app): add GdsInput and GdsSelectbox components with styles and integration

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo';
+
+  options = [
+    { display: '0 - A IDENTIFICAR', value: '0' },
+    { display: 'OO - OUTROS 2', value: 'OO' },
+    { display: '00000 - PADRÃO', value: '00000' },
+    { display: '00000000 - A IDENTIFICAR', value: '00000000' },
+    {
+      display: '00000001 - FRETE SOBRE COMPRA DE MATERIAIS',
+      value: '00000001',
+    },
+  ];
 }
