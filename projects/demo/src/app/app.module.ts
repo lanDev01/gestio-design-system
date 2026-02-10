049a75749a7f967a38bfbcc06ae74c1f8928f4e0
LanDev01 <alnjr0001@outlook.com>
2026-02-09 21:26:24 -0300

    feat(layout): implement header and aside components with styles

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { GdsButtonModule } from '@gestio/gds';
+import { HeaderComponent } from './layout/header/header.component';
+import { AsideComponent } from './layout/aside/aside.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, AsideComponent],
   imports: [BrowserModule, AppRoutingModule, GdsButtonModule],
   providers: [],
   bootstrap: [AppComponent],
