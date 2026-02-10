72237a235dd5cd00b54bf8c59133cded6ee0e593
LanDev01 <alnjr0001@outlook.com>
2026-02-09 22:05:16 -0300

    feat(app): integrate GdsLabel and GdsTextbox components with styles in app layout

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { GdsButtonModule, GdsInputModule } from '@gestio/gds';
+import { GdsButtonModule, GdsInputModule, GdsTextboxModule } from '@gestio/gds';
+import { GdsLabelModule } from 'projects/gds/src/lib/gds-label/gds-label.module';
 import { AsideComponent } from './layout/aside/aside.component';
 import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AsideComponent],
-  imports: [BrowserModule, AppRoutingModule, GdsButtonModule, GdsInputModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GdsButtonModule,
+    GdsInputModule,
+    GdsLabelModule,
+    GdsTextboxModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
